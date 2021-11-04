e042875136e0517392281b7fd63571ea1e183070
Rodolfo Meneguetti <paulo.meneguettitec@gmail.com>
2021-11-03 23:25:33 -0300

    Hello Word

---
+++ src/main/java/br/com/sample/Program.java
@@ -0,0 +1,10 @@
+package br.com.sample;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word !");
+
+	}
+
+}
--- src/main/java/br/com/sample/SampleMavenProjectApplication.java
+++
@@ -1,7 +0,0 @@
-package br.com.sample;
-
-public class SampleMavenProjectApplication {
-    public static void main(String... args) {
-        System.out.println(" ==== \n Simples Projecto com Maven! \n ====");
-      }
-}
