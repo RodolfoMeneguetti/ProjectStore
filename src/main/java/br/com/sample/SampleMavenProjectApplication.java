ec560e30548cdcd612b8fbe6cf66566ced4e4cd4
Rodolfo Meneguetti <paulo.meneguettitec@gmail.com>
2021-11-03 23:14:09 -0300

    Initial Project

---
+++ src/main/java/br/com/sample/SampleMavenProjectApplication.java
@@ -0,0 +1,7 @@
+package br.com.sample;
+
+public class SampleMavenProjectApplication {
+    public static void main(String... args) {
+        System.out.println(" ==== \n Simples Projecto com Maven! \n ====");
+      }
+}
