e927581d47c8a85444513d74adce0ef2b8ec95a7
paco soares <pascoal_soares@goodyear.com>
2021-11-05 18:05:48 -0300

    Basic imput of products

--- src/main/java/br/com/Application/Program.java
+++ src/main/java/br/com/Application/Program.java
@@ -3,14 +3,29 @@ package br.com.Application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import br.com.controller.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		Scanner sc =  new Scanner (System.in); 
 		Locale.setDefault(Locale.US);
-		
+		Scanner sc =  new Scanner (System.in); 
+		System.out.println("Digite a categoria:");
+		String categoria = sc.next();
+		System.out.println("Digite a descrição:");
+		String descrição = sc.next();
+		System.out.println("Digite o valor:");
+		Double valor = sc.nextDouble();
+		sc.close();
+		Product prod = new Product();
+		prod.setCategory(categoria);
+		prod.setDescription(descrição);
+		prod.setValue(valor);
 		System.out.println("Welcome System!");
-		
+		System.out.println("O ítem "+ prod.getDescription() + ","
+				+ " da categoria = "+ prod.getCategory() + 
+				" com o valor de R$"+prod.getValue() + 
+				" foi incluído com sucesso!");
 	
 	}
 
